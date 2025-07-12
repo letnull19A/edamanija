65645ac6e3f6269cb9f76c4fedbaedcc3e1a9c86
xarlein <alexvolkovdd@gmail.com>
2025-07-12 14:27:51 +0400

    feat: added link component and new page registration

--- frontend/src/app/auth/ui/AuthForm.tsx
+++ frontend/src/app/auth/ui/AuthForm.tsx
@@ -2,6 +2,7 @@ import PasswordField from '@/components/PasswordField'
 import Panel from '@/components/Panel'
 import TextField from '@/components/TextField'
 import Button from '@/components/Button'
+import Link from '@/components/Link'
 import style from './style.module.css'
 
 export default function AuthForm() {
@@ -11,6 +12,7 @@ export default function AuthForm() {
       <TextField title='Введите ваш логин' placeholder='Лоин или почту' />
       <PasswordField title='Введите ваш пароль' placeholder='Пароль' />
       <Button className={style.authButton} label='Войти' />
+      <Link href={'/registration'}>Нет аккаунта</Link>
     </Panel>
   )
 }
---
+++ frontend/src/app/registration/ui/RegistrationForm.tsx
@@ -0,0 +1,20 @@
+import PasswordField from '@/components/PasswordField'
+import Panel from '@/components/Panel'
+import TextField from '@/components/TextField'
+import Button from '@/components/Button'
+import Link from '@/components/Link'
+import style from './style.module.css'
+
+export default function RegistrationForm() {
+  return (
+    <Panel className={style.registrationPanel}>
+      <h1 className={style.registrationTitle}>Регистрация</h1>
+      <TextField title='Ваш логин' placeholder='Логин из 5-8 символов' />
+      <TextField title='Ваш e-mail' placeholder='Действующий e-mail' />
+      <PasswordField title='Ваш пароль' placeholder='Пароль' />
+      <PasswordField title='Подтвердите пароль' placeholder='Тоже самое что выше вводили' />
+      <Button className={style.registrationButton} label='Зарегистрироваться' />
+      <Link href={'/registration'}>Есть аккаунт, войти</Link>
+    </Panel>
+  )
+}
