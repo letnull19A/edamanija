697bbd8f1e8eb09c88519f29db1ed562856921fb
letnull19a <letnull19a@gmail.com>
2025-08-19 01:43:56 +0400

    feat: added service with drafts

--- backend/src/category/category.controller.ts
+++ backend/src/category/category.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common'
+import {
+  Controller,
+  Body,
+  Get,
+  Put,
+  Post,
+  Delete,
+  Param,
+  Version,
+  Logger,
+  BadRequestException,
+} from '@nestjs/common'
 
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+  private readonly logger: Logger
+
+  constructor() {
+    this.logger = new Logger(CategoryController.name)
+  }
+
+  @Version('1')
+  @Get()
+  private async getAllV1(): Promise<Array<object>> {
+    return new Promise((resolve) => resolve([]))
+  }
+
+  @Version('2')
+  @Get()
+  private async getAllV2(): Promise<Array<object>> {
+    return new Promise((resolve) => resolve([]))
+  }
+}
