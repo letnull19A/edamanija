3643047fc471c322a199c61e78973d071e8c3bd5
letnull19A <alexvolkovdd@googlemail.com>
2025-08-20 02:51:28 +0400

    feat: добавил обработчик для контроллеров

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config'
 import { AppService } from './app.service'
 import { UserModule } from './../user/user.module'
 import { AuthModule } from './../auth/auth.module'
+import { CategoryModule } from './../category/category.module'
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { AuthModule } from './../auth/auth.module'
     }),
     UserModule,
     AuthModule,
+    CategoryModule,
   ],
   providers: [AppService],
 })
--- backend/src/category/category.controller.ts
+++ backend/src/category/category.controller.ts
@@ -10,19 +10,22 @@ import {
   Logger,
   BadRequestException,
 } from '@nestjs/common'
+import { CategoryService } from './category.service'
 
 @Controller('category')
 export class CategoryController {
   private readonly logger: Logger
 
-  constructor() {
+  constructor(
+    private readonly categoryService: CategoryService,
+  ) {
     this.logger = new Logger(CategoryController.name)
   }
 
   @Version('1')
   @Get()
   private async getAllV1(): Promise<Array<object>> {
-    return new Promise((resolve) => resolve([]))
+    return await this.categoryService.getAll()
   }
 
   @Version('2')
@@ -34,6 +37,18 @@ export class CategoryController {
   @Version('1')
   @Put()
   public async editV1(): Promise<object> {
+    return new Promise((resolve) => resolve({}))
+  }
+
+  @Version('1')
+  @Delete()
+  public async deleteV1(): Promise<object> {
+    return new Promise((resolve) => resolve({}))
+  }
+
+  @Version('1')
+  @Post()
+  public async createV1(): Promise<object> {
     return new Promise((resolve) => resolve({}))
   }
 }
