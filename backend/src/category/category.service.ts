ab250c97aac07bf4cf6f2951b75370ff29095a0a
Alex <68811141+letnull19A@users.noreply.github.com>
2025-08-19 04:27:52 +0400

    Backend category (#55)
    * feat: added service with drafts
    
    * feat: добавлен подмодуль .vimrc
    
    * refactor: все тесты категорий перемещены в __tests__
    
    * feat: документация к функциям
    
    ---------
    
    Co-authored-by: letnull19a <letnull19a@gmail.com>

---
+++ backend/src/category/category.service.ts
@@ -0,0 +1,32 @@
+import { Injectable, Logger } from '@nestjs/common'
+
+@Injectable()
+export class CategoryService {
+  private readonly logger: Logger
+
+  constructor() {
+    this.logger = new Logger(CategoryService.name)
+  }
+
+  public async getAll(): Promise<Array<object>> {
+    return new Promise((resolve) => resolve([]))
+  }
+
+  /***
+   * @description - создаёт категорию
+   */
+  public async create(): Promise<object> {
+    return new Promise((resolve) => resolve({}))
+  }
+
+  /***
+   * @description - удаляет категорию
+   */
+  public async remove(): Promise<object> {
+    return new Promise((resolve) => resolve({}))
+  }
+
+  public async edit(): Promise<object> {
+    return new Promise((resolve) => resolve({}))
+  }
+}
