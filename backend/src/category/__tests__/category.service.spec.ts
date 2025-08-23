507589702fe8156605170ec0b0f12c0cfd7d45f2
letnull19A <alexvolkovdd@googlemail.com>
2025-08-23 22:38:11 +0400

    feat: сделал тесты в виде фабкики и оно работает :P

--- backend/src/category/__tests__/category.service.spec.ts
+++ backend/src/category/__tests__/category.service.spec.ts
@@ -1,5 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing'
-import { CategoryService } from './category.service'
+import { CategoryService } from './../category.service'
+import { CommonErrorFormat } from './../../libs/cef'
+import { readFileSync } from 'fs'
+
+//FIXME: перенести типы в общую библиотеку
+type TFixtureResult = {
+  length?: number
+  errorMessageCollection: Array<string>
+}
+
+type TFixture = {
+  description: string
+  input: object
+  expect: TFixtureResult
+}
+
+//FIXME: нужен рефакторинг
+/**
+* @description - читает json файл и преобразует в тестовые данные
+* */
+const readFixture = (fileName: string): Array<any> => {
+  return JSON.parse(
+    readFileSync(
+      [__dirname, 'fixtures', fileName + '.json'].join('/'),
+      'utf-8',
+    ),
+  ) as Array<TFixture>
+}
+
+/**
+ * @description - фабрика для тестов
+ * */
+const testFixtureFactory = (fileName: string) => {
+  const data = readFixture(fileName)
+
+  return describe.each(data)(
+    fileName,
+    (data) => {
+     it(data.description, () => {
+      expect(data.expect).toEqual(data.expect) 
+     })
+    }
+  )
+}
 
 describe('CategoryService', () => {
   let service: CategoryService
@@ -16,4 +59,6 @@ describe('CategoryService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined()
   })
+
+  testFixtureFactory('fn.getAll')
 })
